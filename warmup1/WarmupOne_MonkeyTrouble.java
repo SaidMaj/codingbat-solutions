4d5d59682e7de71bd27a5febe7275027d75eea50
SaidMaj <saidalmamri9999@gmail.com>
2025-07-24 17:34:42 +0400

    Add solution for MonkeyTrouble problem

---
+++ warmup1/WarmupOne_MonkeyTrouble.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class WarmupOne_MonkeyTrouble {
+    public static void main(String[] args) {
+        System.out.println(monkeyTrouble(true,true));
+    }
+
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return aSmile == bSmile;
+    }
+
+}
