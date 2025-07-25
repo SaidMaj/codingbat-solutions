0c97bf4ad661f06cad7a8284a349f2db97e71ab4
SaidMaj <saidalmamri9999@gmail.com>
2025-07-25 17:14:54 +0400

    Add solution for WarmupOne_StringE.java

---
+++ warmup1/WarmupOne_StringE.java
@@ -0,0 +1,18 @@
+public class WarmupOne_StringE{
+  public static void main(String args[]) {
+        System.out.println(stringE("Hello World"));
+  }
+
+public static boolean stringE(String str) {
+  
+  int count = 0;
+  
+  for(int i = 0 ; i < str.length(); i++)
+  {
+    if(str.charAt(i) == 'e')
+      count++;
+  }
+
+  return count > 0 && count <= 3; 
+}
+}
