d3f906e74e097abfb6dcd20e00f65693f9a94f1e
SaidMaj <saidalmamri9999@gmail.com>
2025-07-25 06:29:48 +0400

    Add solution for WarmupOne_In1020.java

---
+++ warmup1/WarmupOne_In1020.java
@@ -0,0 +1,11 @@
+public class WarmupOne_in1020 {
+  public static void main(String args[]) {
+        System.out.println(in1020(5,4));
+  }
+  
+public static boolean in1020(int a, int b) {
+  return (a > 9 && a  < 21) ||  (b > 9 && b  < 21);
+}
+
+
+}
