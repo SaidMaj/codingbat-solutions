37be4338adc9a9d785e966a040423def005e12bf
SaidMaj <saidalmamri9999@gmail.com>
2025-07-29 09:29:28 +0400

    logic1 : add solution for love6 problem

---
+++ logic-1/logic1_Love6.java
@@ -0,0 +1,9 @@
+public class logic1_Love6 {
+    public static void main(String args[]) {
+        System.out.println(love6(9, 16));
+    }
+  
+   public static boolean love6(int a, int b) {
+      return a == 6 || b == 6 || a + b == 6 || Math.abs(a - b) == 6;
+   }
+}
