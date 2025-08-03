33a7d891dbca996954f1585ece485aa22ed6877c
SaidMaj <saidalmamri9999@gmail.com>
2025-08-03 22:52:00 +0400

    string3 : adding solution for sumDigits problem

---
+++ string3/string3_SumDigits.java
@@ -0,0 +1,23 @@
+class string3_SumDigits{
+    public static void main(String[] args) {
+        System.out.println(sumDigits("aa1bc2d3"));
+    }
+    
+    public static int sumDigits(String str) {
+      int sum = 0;
+      
+      for(int i = 0 ; i < str.length(); i++)
+      {
+         if(Character.isDigit(str.charAt(i)) )
+         {
+           // sum += (str.charAt(i) - '0');
+           sum += (Integer.parseInt(str.substring(i, i+1)));
+         }
+            
+      }
+      
+      return sum;
+    }
+
+
+}
