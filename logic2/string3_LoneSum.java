0d70ffb5f2f48b009b0ff699a4fb9e584abf97ac
SaidMaj <saidalmamri9999@gmail.com>
2025-08-03 23:09:46 +0400

    string3 : adding solution for LoneSum problem

---
+++ logic2/string3_LoneSum.java
@@ -0,0 +1,28 @@
+class string3_LoneSum{
+    public static void main(String[] args) {
+        System.out.println(loneSum(1, 2, 3));
+    }
+    
+    public int loneSum(int a, int b, int c) {
+      if(a != b & b != c && a != c)
+        return a + b + c;
+        
+      if (b == c && a != c )
+      {
+        return a ;
+      }
+      
+      if (a == c && b != c )
+      {
+        return b ;
+      }
+      
+      if (a == b && c != a )
+      {
+        return c ;
+      }
+      
+      return 0;
+    }
+
+}
