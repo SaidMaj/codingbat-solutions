71e9a6be27a35408446f0866b969ad20f6710e0a
SaidMaj <saidalmamri9999@gmail.com>
2025-08-03 23:19:28 +0400

    string3 : adding solution for noTeenSum problem

---
+++ logic2/string3_NoTeenSum.java
@@ -0,0 +1,18 @@
+class string3_NoTeenSum{
+    public static void main(String[] args) {
+        System.out.println(noTeenSum(1, 2, 3));
+    }
+    
+    public static int noTeenSum(int a, int b, int c) {
+      return  fixTeen(a) + fixTeen(b) + fixTeen(c);
+    }
+    
+    public static int fixTeen(int n) {
+      if((n >= 13 && n <= 14) || (n >= 17 && n <= 19) )
+      {
+        return 0;
+      }
+      return n;
+    }
+
+}
